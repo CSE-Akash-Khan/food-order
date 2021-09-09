5ff79edabb9fc4ccb94ad66e27a1e3816c82c361
CSE-Akash-Khan <60233974+CSE-Akash-Khan@users.noreply.github.com>
2021-09-09 12:41:56 +0600

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -4,11 +4,17 @@
 
 
     //create constants to store non repeating values
-    define('SITEURL','http://localhost/food-order/');
-    define('LOCALHOST','localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD','');
-    define('DB_NAME','food-order');
+//     define('SITEURL','http://localhost/food-order/');
+//     define('LOCALHOST','localhost');
+//     define('DB_USERNAME', 'root');
+//     define('DB_PASSWORD','');
+//     define('DB_NAME','food-order');
+
+    define('SITEURL','http://sql6.freemysqlhosting.net/sql6435798/');
+    define('LOCALHOST','sql6.freemysqlhosting.net');
+    define('DB_USERNAME', 'sql6435798');
+    define('DB_PASSWORD','F7HZxD1q6K');
+    define('DB_NAME','sql6435798');
     
 
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME,DB_PASSWORD) or die(mysqli_error()); //data base connection.. default user name root and pass: blank
